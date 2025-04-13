be03478104b47b407f9e2e35f2a94bd6dc623e35
DongHae Shin <jeensh25@gmail.com>
2025-04-13 22:58:22 +0900

    Title: 세 정수형 변수 선언, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250413/세 정수형 변수 선언/declaration-of-three-natural-numbers.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String result = "";
+        int a = 7, b = 23, c = 30;
+        result = a + " + " + b + " = " + c;
+
+        System.out.println(result);
+    }
+}
