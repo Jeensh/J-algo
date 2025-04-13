881523a8ad68cae92d2f229f41852f517ee1d369
DongHae Shin <jeensh25@gmail.com>
2025-04-13 23:13:40 +0900

    Title: 점수 입출력, Time: 111ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250413/점수 입출력/enter-int-and-print-score.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int line = sc.nextInt();
+
+        System.out.println("Your score is " + line + " point.");
+    }
+}
