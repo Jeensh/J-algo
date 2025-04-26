d64efd023b02f99b91a312f3537c8a453d0c322a
DongHae Shin <jeensh25@gmail.com>
2025-04-27 02:25:27 +0900

    Title: 일의 자리 배열, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 250427/일의 자리 배열/array-with-units.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] arr = new int[10];
+        arr[0] = sc.nextInt();
+        arr[1] = sc.nextInt();
+
+        for(int i=2; i < 10; i++){
+            arr[i] = (arr[i-1] + arr[i-2]) % 10;
+        }
+
+        for(int x : arr) {
+            System.out.print(x + " ");
+        }
+    }
+}
